beafd7228799ab2620c006defa2cb925c40f86bb
Josman Altamira <78773364+JosmanAltamira@users.noreply.github.com>
2023-01-30 17:41:38 -0600

    controlador api productos , tabla empleados

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,47 @@ Route::get('/consulta', function () {
         echo $producto->nombre;
     }
 });
+
+Route::get('/buscarPorId', function () {
+    $producto = Producto::all();
+    echo $producto->find(2);
+});
+
+Route::get('/buscarPorId2', function () {
+    $producto = Producto::find(2);
+    echo $producto->nombre;
+});
+
+Route::get('/buscarPorId3', function () {
+    $producto = Producto::findOrFail(3);
+    echo $producto->nombre;
+});
+
+Route::get('/insertar',function () {
+    $producto = new Producto;
+    $producto->nombre = 'reloj';
+    $producto->precio = 321;
+    $producto->estado = 1;
+    $producto->save();
+});
+
+Route::get('/actualizar',function () {
+    $producto = Producto::findOrFail(3);
+    $producto->precio = 400;
+    $producto->save();
+});
+
+Route::get('/eliminar',function () {
+    $producto = Producto::findOrFail(3);
+    $producto->delete();
+});
+
+Route::get('/actualizar',function () {
+    $producto = Producto::findOrFail(3);
+    $producto->precio = 400;
+    $producto->save();
+});
+
+//API
+Route::get('/productosApi/index','App\Http\controllers\ProductoApiController@index'); 
+Route::post('/productosApi/store','App\Http\controllers\ProductoApiController@store');
